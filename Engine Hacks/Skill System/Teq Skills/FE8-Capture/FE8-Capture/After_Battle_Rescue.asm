fc50eb2fab89668467b385779518ec298aa61037
boviex <boviex@users.noreply.github.com>
2019-02-14 11:56:50 +0000

    fix items not dropping and scripted battles not ending when one party dies

--- Engine Hacks/Skill System/Teq Skills/FE8-Capture/FE8-Capture/After_Battle_Rescue.asm
+++ Engine Hacks/Skill System/Teq Skills/FE8-Capture/FE8-Capture/After_Battle_Rescue.asm
@@ -14,6 +14,13 @@ mov		r0,r5
 .short	0xF800
 mov r5, r0
 
+ldrb	r7,[r4,#0xB]		@defender allegiance
+ldr		r0,Get_Char_Data
+mov		r14,r0
+mov		r0,r7
+.short	0xF800
+mov r7, r0
+
 ldr		r0,Is_Capture_Set
 mov		r14,r0
 mov		r0,r5
@@ -29,12 +36,12 @@ beq		GoBack
 	and		r0,r1
 	str		r0,[r5,#0xC]		@remove the 'is capturing' bit from attacker
 
-ldrb	r7,[r4,#0xB]		@defender allegiance
-ldr		r0,Get_Char_Data
-mov		r14,r0
-mov		r0,r7
-.short	0xF800
-mov r7, r0
+@ldrb	r7,[r4,#0xB]		@defender allegiance
+@ldr		r0,Get_Char_Data
+@mov		r14,r0
+@mov		r0,r7
+@.short	0xF800
+@mov r7, r0
 
 ldrb	r0,[r4,#0x13]		@defender current hp
 cmp		r0,#0x0
